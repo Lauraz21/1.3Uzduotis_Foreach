f073df8aceaffb633e658ae17d20639be77360aa
Laura <Laura@DESKTOP-BESSB6E>
2023-09-20 20:25:49 +0300

    Add project files.

---
+++ 1.3Uzduotis_Foreach/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1._3Uzduotis_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pays = { 500, 200, 300 };
+            calculateGpm(pays);
+        }
+        public static void calculateGpm(int[] pays)
+        {
+            Console.WriteLine("GPM: ");
+
+            foreach (int item in pays)
+            {
+                if (item < 0)
+                {
+                    return;
+                }
+                Console.WriteLine(item * 0.15);
+            }
+        }
+    }
+}
